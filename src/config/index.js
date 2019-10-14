349422e7fe4b1522ced2127f0545e45870f94a9f
VIMGAS <victormoraesgs@gmail.com>
2019-10-13 23:27:05 -0300

    feat: configuration

---
+++ src/config/index.js
@@ -0,0 +1,47 @@
+const get = require('lodash.get');
+const mergeWith = require('lodash.mergewith');
+const set = require('lodash.set');
+
+class Config {
+  /**
+   * Constructs the Config class
+   *
+   * @param {object} DependencyInjection
+   * @param {import('../di')} DependencyInjection.Container
+   * @param {import('@coobo/app')} DependencyInjection.Application
+   */
+  constructor({ Container, Application, requireAll, esmResolver }) {
+    const configPath = Application.configPath();
+    this._config = requireAll({
+      dirname: configPath,
+      resolve: mod => {
+        mod = esmResolver(mod);
+        if (typeof mod === 'function') mod = mod(Container.cradle);
+        return mod;
+      },
+    });
+  }
+
+  get(key, defaultValue = null) {
+    return get(this._config, key, defaultValue);
+  }
+
+  merge(key, defaultValues, customizer = null) {
+    return mergeWith(defaultValues, this.get(key), customizer);
+  }
+
+  set(key, value) {
+    set(this._config, key, value);
+  }
+
+  defaults(key, value) {
+    const existingValue = this.get(key);
+    if (existingValue) {
+      mergeWith(value, existingValue);
+    }
+
+    this.set(key, value);
+  }
+}
+
+module.exports = Config;
---
+++ tests/unit/config.spec.js
@@ -0,0 +1,68 @@
+import path from 'path';
+
+import config from '../../src/config';
+import container from '../../src/di';
+
+const asClass = container.resolve('asClass');
+container.register({
+  Application: asClass(
+    class App {
+      configPath() {
+        return path.join(__dirname, 'config-path');
+      }
+    },
+  ),
+  Config: asClass(config),
+});
+
+let Config;
+
+beforeEach(() => {
+  Config = container.resolve('Config');
+});
+
+describe('Config Class', () => {
+  it('should use provided default when config does not exist', () => {
+    expect(Config.get('aaa', 'teste')).toBe('teste');
+  });
+
+  it('should set a config', () => {
+    Config.set('aaa', 'ABCD');
+
+    expect(Config.get('aaa')).toBe('ABCD');
+  });
+
+  it('should set default configs', () => {
+    Config.defaults('tests', {
+      a: 'b',
+      b: 'c',
+      c: { d: 'e' },
+    });
+
+    expect(Config.get('tests.a')).toBe('b');
+    expect(Config.get('tests.b')).toBe('c');
+    expect(Config.get('tests.c.d')).toBe('e');
+  });
+
+  it('should have autoloaded config-path', () => {
+    expect(Config.get('app')).toStrictEqual({ config: 'test', oneTest: true });
+    expect(Config.get('app.config')).toBe('test');
+    expect(Config.get('app.oneTest')).toBe(true);
+  });
+
+  it('should not overwrite when passing defaults with already existing key', () => {
+    Config.defaults('app', {
+      config: 'not-test',
+      porta: 1001,
+    });
+
+    expect(Config.get('app.config')).toBe('test');
+    expect(Config.get('app.oneTest')).toBe(true);
+    expect(Config.get('app.porta')).toBe(1001);
+  });
+
+  it('should use container to resolve functional configs', () => {
+    expect(Config.get('function.conf')).toBe(container);
+    expect(typeof Config.get('function.conf').resolve).toBe('function');
+  });
+});
