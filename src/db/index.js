2e9edeb6f33c428ec53e248be685cce755a2b860
VIMGAS <victormoraesgs@gmail.com>
2019-11-06 21:14:26 -0300

    feat: add new database interface

--- src/db/index.js
+++ src/db/index.js
@@ -1,49 +1,155 @@
-const mongoose = require('mongoose');
+import mongoose from 'mongoose';
 
-/**
- * Factory function that requires @coobo/di to get injected dependencies and
- * returns a Mongoose connected instance.
- *
- * @function DatabaseFactory
- *
- * @binding DatabaseFactory
- * @alias DatabaseFactory
- * @group Database
- * @singleton
- *
- * @param {object} Dependencies
- * @param {import('@coobo/config').Config} Dependencies.Config
- * @param {import('@coobo/log').Logger} Dependencies.Logger
- *
- * @returns {import('mongoose').Mongoose}
- */
-function DatabaseFactory({ Config, Logger }) {
-  const db = new mongoose.Mongoose();
-  db.connect(
-    Config.get('db.uri'),
-    Config.get('db.options', {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      autoReconnect: true,
-      reconnectTries: Number.MAX_VALUE,
-      reconnectInterval: 1000,
-    }),
-  );
+import config from '../config';
+import env from '../env';
+import logger from '../logger';
+import dbUri from '../utils/db-uri';
+import esmRequire from '../utils/esm-require';
 
-  db.connection.on('error', e => {
-    Logger.error({ message: 'Error whith the database connection.', error: e });
-  });
+const DB_HOST = env.get('DB_HOST');
+const DB_PORT = env.get('DB_PORT');
+const DB_USER = env.get('DB_USER');
+const DB_PASS = env.get('DB_PASS');
+const DB_NAME = env.get('DB_NAME');
+const DB_URI = env.get(
+  'DB_URI',
+  dbUri(DB_HOST, DB_PORT, DB_NAME, DB_USER, DB_PASS),
+);
 
-  db.connection.once('open', () =>
-    Logger.debug(
-      `${Config.get(
-        'app.name',
-        'Application',
-      )} successfully connected to MongoDB`,
-    ),
-  );
+config.defaults('db', {
+  uri: DB_URI,
+  host: DB_HOST,
+  port: DB_PORT,
+  name: DB_NAME,
+  user: DB_USER,
+  password: DB_PASS,
+  options: {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    autoReconnect: true,
+    reconnectTries: Number.MAX_VALUE,
+    reconnectInterval: 1000,
+  },
+});
 
-  return db;
-}
+const Database = {
+  /**
+   * The Database connection
+   *
+   * @type {mongoose.Connection}
+   */
+  _connection: new mongoose.Mongoose(),
 
-module.exports = DatabaseFactory;
+  /**
+   * The Database URI to connect to.
+   *
+   * @type {string}
+   * @private
+   */
+  _uri: config.get('db.uri'),
+
+  /**
+   * The Database connection options.
+   *
+   * @type {object}
+   * @private
+   */
+  _options: config.get('db.options'),
+
+  /**
+   * Internal loaded models.
+   *
+   * @type {Object<string,mongoose.Model>}
+   */
+  models: {},
+
+  /**
+   * Internal used mongoose.
+   *
+   * @type {mongoose}
+   */
+  mongoose,
+
+  /**
+   * Connects to the database using the config provider.
+   *
+   * @returns {void}
+   */
+  connect() {
+    logger.profile('MONGODB_CONNECT');
+    this._connection.connect(this._uri, this._options);
+
+    this._connection.connection.on('error', error => {
+      logger.error({
+        msg: 'There was an error with the database connection.',
+        error,
+      });
+    });
+
+    this._connection.connection.once('open', () => {
+      logger.profile('MONGODB_CONNECT', 'debug');
+      logger.debug(
+        `${config.get(
+          'app.name',
+          'Application',
+        )} successfully connected to MongoDB`,
+      );
+    });
+  },
+
+  /**
+   * Gets the specified mongoose model.
+   *
+   * @param {string} name
+   *
+   * @returns {mongoose.Model}
+   */
+  get(name) {
+    if (name.includes('Model')) name = name.replace('Model', '');
+
+    return this.models[name];
+  },
+
+  /**
+   * Gets the specified mongoose model.
+   *
+   * @param {string} name
+   *
+   * @returns {mongoose.Model}
+   */
+  add(model, name = null) {
+    if (!name) name = model.modelName;
+
+    this.models[name] = model;
+  },
+
+  /**
+   * Checks if the database has the specified mongoose model.
+   *
+   * @param {string} name
+   *
+   * @returns {boolean}
+   */
+  has(name) {
+    if (name.includes('Model')) name = name.replace('Model', '');
+
+    return this.models[name] !== undefined;
+  },
+
+  /**
+   * Loads a new model to the database.
+   *
+   * @param {string} modelPath
+   * @param {string} [name=null]
+   *
+   * @returns {void}
+   */
+  load(modelPath, name = null) {
+    const model = esmRequire(modelPath)(this._connection, this.models);
+    if (!name) name = model.modelName;
+
+    this.models[name] = model;
+  },
+};
+
+export default Database;
