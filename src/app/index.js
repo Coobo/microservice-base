53f9438f3f06579930b894481df32169a285a9d8
VIMGAS <victormoraesgs@gmail.com>
2019-11-07 21:26:18 -0300

    fix: boot function not having full access to app test env

--- src/app/index.js
+++ src/app/index.js
@@ -1,17 +1,19 @@
 import config from '../config';
 import { appRoot as APP_ROOT, pathTo } from '../utils/root';
 
-const ENV = config.get('env');
-
 const PACKAGE = require(pathTo('package.json'));
 
 config.defaults('app', {
   root: APP_ROOT,
-  folder: ['development', 'testing'].includes(ENV) ? 'src' : 'dist',
-  inTest: ENV === 'testing',
-  inProduction: ENV === 'production',
-  inStaging: ENV === 'staging',
-  inDev: ENV === 'development',
+  folder: ['development', 'testing'].includes(
+    config._env.get('NODE_ENV', 'development'),
+  )
+    ? 'src'
+    : 'dist',
+  inTest: config._env.get('NODE_ENV', 'development') === 'testing',
+  inProduction: config._env.get('NODE_ENV', 'development') === 'production',
+  inStaging: config._env.get('NODE_ENV', 'development') === 'staging',
+  inDev: config._env.get('NODE_ENV', 'development') === 'development',
   name: 'spendfy-service',
   paths: {
     domains: 'app/domains',
@@ -21,7 +23,7 @@ config.defaults('app', {
 const Application = {
   root: config.get('app.root'),
   folder: config.get('app.folder'),
-  env: config.get('env'),
+  env: config._env.get('NODE_ENV', 'development'),
   inTest: config.get('app.inTest'),
   inProd: config.get('app.inProduction'),
   inStage: config.get('app.inStaging'),
@@ -49,7 +51,7 @@ const Application = {
   load() {
     this.root = config.get('app.root');
     this.folder = config.get('app.folder');
-    this.env = config.get('env');
+    this.env = config._env.get('NODE_ENV', 'development');
     this.inTest = config.get('app.inTest');
     this.inProd = config.get('app.inProduction');
     this.inStage = config.get('app.inStaging');
